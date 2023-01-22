57c5c5914fec160fdc58996c7216e562b49a43a2
Marina <markovshut@gmail.com>
2023-01-22 23:24:31 +1000

    задание 1

--- js/script.js
+++ js/script.js
@@ -1,75 +1,20 @@
-/* console.log(und); */
-
-/* const obj = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
- */
-//console.log(obj.name);
-
-//console.log(obj["age"]);
-
-//let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp'];
-//console.log(arr[1])
-/* 
-const arr = ['a', 'b', 'c'];
-arr[10] = '3456';
-console.log(arr[10]);
-
-const arrObj = {
-    0: 'a',
-    1: 'b',
-    2: 'c',
-    abc: {
-        df: [{}, {}],
-        def: {
-
-        } 
-    }
-};
-
-arrObj.b = '1234';
-
-console.log(arrObj['b']);
-console.log(arrObj.b); */
-
-/* alert('hello'); */
-/* const result = confirm("Are you here?");
-console.log(result); */
-/* const answer = prompt("Вам есть 18?", "");
-console.log(answer); */
-
-/* const answers = [];
-
-answers[0] = prompt('Как ваше Имя?');
-answers[1] = prompt('Как ваша фамилмя?');
-answ ers[2] = prompt('Сколько вам лет?');
-
-document.write(answers); */
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-/* const category = 'toys';
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(`https://someurl.com/${category}/5`);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-const user = "marina";
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-alert(`hello, ${user}`); */
-
-/* console.log('arr' + "object");
-console.log(5 - +"5"); */
-/* 
-let incr = 10,
-    decr = 10;
- */
-/* ++incr;
---decr; */
-
-/* console.log(++incr);
-console.log(--decr); */
-
-const inChecked = false,
-    isClouse = false;
-
-console.log(inChecked || !isClouse);
+console.log(personalMovieDB);
