25c993706c80751a6893d6eccdab041ab0339c03
Marina <markovshut@gmail.com>
2023-01-24 00:15:21 +1000

    логические операторы "===" ,"&&", "||", "!"

--- js/script.js
+++ js/script.js
@@ -1,55 +1,11 @@
 "use strict";
-/* const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB); */
-
-/* if (5 == 9) {
-    console.log('ok!');
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log('все сыты');
 } else {
-    console.log('error!');
-}
-
-const num = 50; */
-/* 
-if (num < 49) {
-    console.log('error!');
-} else if (num > 100) {
-    console.log('mnogo');
-} else {
-    console.log('ok');
-}
-
-(num === 50) ? console.log('ok!'): console.log('error!'); */
-
-const num = 50;
-
-switch (num) {
-    case 49:
-        console.log('не верно');
-        break;
-    case 100:
-        console.log('не верно');
-        break;
-    case 50:
-        console.log('в точку');
-        break;
-    default:
-        console.log('не в этот раз');
-        break;
+    console.log('мы уходим');
 }
