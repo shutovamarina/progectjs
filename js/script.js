48d9176db6ba772443e5ecd26ebab0a89b8615f7
Marina <markovshut@gmail.com>
2023-02-01 12:59:02 +1000

    логические операторы

--- js/script.js
+++ js/script.js
@@ -9,3 +9,46 @@ if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
 } else {
     console.log('мы уходим');
 }
+
+
+
+console.log(NaN || 2 || undefined);
+console.log(NaN && 2 && undefined);
+
+
+console.log(1 && 2 && 3);
+
+
+console.log(!1 && 2 || !3);
+
+
+console.log(25 || null && !3);
+
+
+console.log(NaN || null || !3 || undefined || 5);
+
+
+console.log(NaN || null && !3 && undefined || 5);
+
+
+console.log(5 === 5 && 3 > 1 || 5);
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger || cola || fries === 3 || nuggets) {
+    console.log('Done!')
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+    console.log('Done!')
+}
