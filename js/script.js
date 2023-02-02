68c8a0b20b3d1513d6c47ddeb260e601ca6d20e9
Marina <markovshut@gmail.com>
2023-02-02 15:03:23 +1000

    задачки

--- js/script.js
+++ js/script.js
@@ -1,45 +1,86 @@
 "use strict";
 
-let num = 50;
-
-/* while (num <= 55) {
-    console.log(num);
-    num++
-} */
-
-do {
-    console.log(num);
-    num++
-}
-while (num < 55);
-
-for (let i = 1; i < 12; i++) {
-    if (i === 7) {
-        break;
+// Место для первой задачи
+function firstTask() {
+    for (let i = 5; i < 11; i++) {
+        console.log(i);
     }
-    console.log(i);
 }
 
-let result = '';
-const leght = 7;
-
-for (let i = 1; i < leght; i++) {
-
-    for (let j = 0; j < i; j++) {
-        result += "*";
+// Место для второй задачи
+function secondTask() {
+    for (let i = 20; i > 9; i--) {
+        if (i === 13) break;
+        console.log(i);
     }
-    result += '\n';
+
+
 }
-console.log(result);
+
+// Место для третьей задачи
+function thirdTask() {
+    for (let i = 2; i < 11; i++) {
+        if (i % 2 == 0)
+            console.log(i);
+    }
 
 
-first: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) continue first;
-            console.log(`Third level: ${k}`);
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let i = 2;
+
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+
+
+
+function fourthTask() {
+    let i = 2;
+
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
         }
+        i++;
     }
+
+
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+
+
+    // Не трогаем
+    return arrayOfNumbers;
 }
