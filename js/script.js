da476f77ac5071f5c164b2f9ab4d78e29f981ad1
Marina <markovshut@gmail.com>
2023-05-11 12:48:37 +1000

    массивы

--- js/script.js
+++ js/script.js
@@ -1,38 +1,32 @@
 'use strict';
 
-const options = {
-    name: 'test',
-    width: 1024,
-    hight: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log("Test");
-    }
-};
+const arr = [1, 26, 13, 6, 18];
+arr.sort(compareNum);
+console.log(arr);
 
-options.makeTest();
+function compareNum(a, b) {
+    return a - b;
+}
+/* arr[99] = 0;
+console.log(arr.length);
+console.log(arr);
+ */
+/* arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива`)
+}) */
+/* 
+arr.pop();
 
-const { border, bg } = options.colors;
-console.log(border);
+console.log(arr); */
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+} */
 
-/* console.log(Object.keys(options).length); */
-/* console.log (options.name);
+/* for (let value of arr) {
+    console.log(value);
+} */
 
-delete options.name;
-console.log (options); */
-/* let counter = 0;
-for (let key in options) {
-    if (typeof(options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`свойство ${i} имеет значение ${options[key] [i]}`);
-            counter++;
-        }
-    } else {
-        console.log(`свойство ${key} имеет значение ${options[key]}`);
-        counter++;
-    }
-};
-console.log(counter); */
+/* const str = prompt("", "");
+const products = str.split(",");
+products.sort();
+console.log(products.join('; ')); */
